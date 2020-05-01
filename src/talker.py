b5e11e65955b1314a32c616c1e38277c13906129
CCasale96 <christian.casale@outlook.com>
2020-05-01 19:18:14 +0200

    sterza

--- src/talker.py
+++ src/talker.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
+from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 from sys import exit
 import readchar
+
+a = 0.11 #interasse delle ruote
+b = 0.25 #passo delle ruote
 
 
 
@@ -11,18 +16,44 @@ def publish_callback(msg):
     rospy.loginfo(msg)
     pub.publish(msg)
 
-def inserimento(comando,msg):
+def inserimento(comando,msg,deltasx,deltadx):
     if comando ==('w'):
+        if msg.linear.x < 0: 
+            msg.linear.x = 0
         msg.linear.x = msg.linear.x + 0.1
     elif comando == ('a'):
-        msg.angular.z = msg.angular.z - 0.1
+ #       steering = steering + 0.1
+        if deltasx > 0.7:
+            deltasx = 0.7
+        else:
+            deltasx = deltasx +0.1
+        
+        deltadx = np.arctan(np.tan(deltasx)+b/a)
+        if deltadx > 0.7: 
+            deltadx = 0.7
+
+    #      deltasx = deltasx +0.2
+    #     deltadx = deltadx +0.1
     elif comando == ('s'):
+        if msg.linear.x > 0: 
+            msg.linear.x = 0
         msg.linear.x = msg.linear.x - 0.1
     elif comando == ('d'):
-        msg.angular.z = msg.angular.z + 0.1
+        if deltadx < -0.7:
+            deltadx = -0.7
+        else:
+            deltadx = deltadx -0.1
+
+        deltasx = np.arctan(b/a-np.tan(deltadx))
+        if deltasx < -0.7:
+            deltasx = -0.7
+
+#        steering = steering -0.1
+#        deltadx = deltadx -0.2
+#        deltasx = deltasx -0.1
     else:
         exit()
-    return msg
+    return msg,deltasx,deltadx
 
 
 if __name__ == '__main__':
@@ -34,14 +65,28 @@ if __name__ == '__main__':
     msg.angular.x = 0.0
     msg.angular.y = 0.0
     msg.angular.z = 0.0
+
+    deltasx = Float64
+    deltasx = 0.0
+    deltadx = Float64
+    deltadx = 0.0
+
     while True:
         rospy.init_node('talker', anonymous=True)
-        pub = rospy.Publisher('/r2d2_diff_drive_controller/cmd_vel', Twist, queue_size=10)
-        #timer = rospy.Timer(rospy.Duration(1. / 10), publish_callback(msg))  # 10Hz
+
+        pub = rospy.Publisher('/posteriori/cmd_vel', Twist, queue_size=10)
         publish_callback(msg)
+        pub = rospy.Publisher('/sinistra/command', Float64, queue_size=10)
+        publish_callback(deltasx)
+        pub = rospy.Publisher('/destra/command', Float64, queue_size=10)
+        publish_callback(deltadx)
+
+
+        #timer = rospy.Timer(rospy.Duration(1. / 10), publish_callback(msg))  # 10Hz
+     
         #rospy.spin()
         #comando = raw_input('Input W-A-S-D per il controllo del robot:\n')
         comando = readchar.readchar()
-        msg = inserimento(comando,msg)
+        msg,deltasx,deltadx = inserimento(comando,msg,deltasx,deltadx)
     #except rospy.ROSInterruptException:
      #   pass
