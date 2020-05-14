7b100aa1636011423fdc1065902b2aba97db5e22
CCasale96 <christian.casale@outlook.com>
2020-05-14 10:31:42 +0200

    finalmente gira

--- src/trajectory2.py
+++ src/trajectory2.py
@@ -10,7 +10,6 @@ import numpy as np
 #from unicycle import unicycle_error_model, control
 from scipy.integrate import odeint
 from io_linearization import io_linearization_control_law
-from trajectory_generation import Trajectory_generation
 
 
 
@@ -34,6 +33,7 @@ class Trajectory_control():
     err = []
     a = 0.21
     b = 0.25
+    b_meter = b/10
     v = 0
     w = 0
     rate = 0
@@ -47,7 +47,6 @@ class Trajectory_control():
         self.twist_pub = rospy.Publisher('/posteriori/cmd_vel', Twist, queue_size=10) 
         self.left_pub = rospy.Publisher('/sinistra/command', Float64, queue_size=10)
         self.right_pub = rospy.Publisher('/destra/command', Float64, queue_size=10)
-        #rospy.Subscriber('move_base_simple/goal', PoseStamped, self.on_goal)
         rospy.Subscriber('/ground_truth/state',Odometry, self.odometryCb)
     
     #current robot pose
@@ -69,20 +68,66 @@ class Trajectory_control():
         theta = yaw
         return theta
 
-    def trajectory_generation(self, trajectory):
-        tg = Trajectory_generation()
-        if(trajectory == "cubic"):
-            #Cubic_trajectory
-            q_i = np.array([0.,0., 3.14/2]) #Initial posture (x_i,y_i,theta_i)
-            q_f = np.array([3.,6., 0.])    #Final posture   (x_f,y_f,theta_f)
-            init_final_velocity = 2
-            (self.x_d, self.y_d, self.v_d, self.w_d, self.theta_d) = tg.cubic_trajectory(q_i, q_f, init_final_velocity, self.t)   
-        elif(trajectory == "eight"):
-            #Eight trajectory
-            (self.x_d, self.y_d, self.dotx_d, self.doty_d) = tg.eight_trajectory(self.t)
-        elif (trajectory == "cyrcular"):    
-            #Cyrcular_trajectory
-            (self.x_d, self.y_d, self.v_d, self.w_d, self.theta_d, self.dotx_d, self.doty_d) = tg.cyrcular_trajectory(self.t)
+
+    def trajectory_generation(self):
+        
+        (self.x_d, self.y_d, self.v_d, self.w_d, self.theta_d, self.dotx_d, self.doty_d) = self.trajectory()
+
+
+
+    def trajectory(self):
+        
+        A = [0.0, 0.0]
+        B = [0.499, 0.021]
+        C = [0.995, 0.085]  
+        
+        var = np.array([[A[0],A[1],1], [B[0],B[1],1], [C[0],C[1],1]])
+
+        notA = -(A[0]**2)-(A[1]**2)
+        notB = -(B[0]**2)-(B[1]**2)
+        notC = -(C[0]**2)-(C[1]**2)
+        noti = np.array([notA, notB, notC])
+        soluzione = np.linalg.solve(var, noti)
+
+        R = np.sqrt((soluzione[0]**2/4)+(soluzione[1]**2/4)-soluzione[2])
+
+        v_d_val = 0.5 # m/s
+        w_d_val = v_d_val/R
+
+        x_c = -soluzione[0]/2
+        y_c = -soluzione[1]/2
+
+        """        
+        x_d = [0.0, 0.249, 0.499, 0.747, 0.995, 1.240, 1.483, 1.723, 1.960, 2.193, 2.384, 2.159]
+        y_d = [0.0, 0.005, 0.021, 0.048, 0.085, 0.134, 0.192, 0.262, 0.341, 0.430, 0.512, 0.403]
+        """
+
+        x_d = R * np.cos(w_d_val * self.t) + x_c
+        y_d = R * np.sin(w_d_val * self.t) + y_c
+        dotx_d = -R*w_d_val*np.sin(w_d_val* self.t)
+        doty_d =  R*w_d_val*np.cos(w_d_val* self.t)
+
+        i = 0
+        k = []
+        for element in x_d:
+            if x_d[i] < 0 or y_d[i] < 0 or y_d[i] > 2.15: 
+                k.append(i)
+            i = i+1
+        
+        x_d = np.delete(x_d,k)
+        y_d = np.delete(y_d,k)
+        dotx_d = np.delete(dotx_d,k)
+        doty_d = np.delete(doty_d,k)
+
+        print(np.shape(x_d))
+        print(y_d)
+
+        v_d = np.sqrt(dotx_d**2 + doty_d**2)
+        theta_d = np.arctan2(doty_d, dotx_d)
+        w_d = w_d_val * np.ones(len(self.t))
+
+        return [x_d, y_d, v_d, w_d, theta_d, dotx_d, doty_d]
+
 
 
     def get_point_coordinate(self, b):
@@ -101,10 +146,9 @@ class Trajectory_control():
         rospy.sleep(0.1) #wait to fill q cinfiguration
         max_t = self.t[len(self.t) - 1]
         len_t = len(self.t)
-        for i in np.arange(0, len(self.t)):
+        for i in np.arange(0, len(self.x_d)):
             (y1, y2, theta) = self.get_point_coordinate(b)
             (self.v, self.w) = io_linearization_control_law(y1, y2, theta, self.x_d[i], self.y_d[i], self.dotx_d[i], self.doty_d[i], b)
-            #print("linear:{} and angular:{}".format(self.v, self.w))
             err = self.get_error(i)
             print(err)
             #move robot
@@ -123,8 +167,7 @@ class Trajectory_control():
     def get_error(self, T):
         #slide 80 LDC
         (x, y, theta) = self.get_pose()
-        #rospy.loginfo("x={} y={} th={}".format(x,y,theta))
-        #compute error
+      
         e1 = (self.x_d[T] - x) * np.cos(theta) + (self.y_d[T] - y) * np.sin(theta)
         e2 = -(self.x_d[T] - x) * np.sin(theta) + (self.y_d[T] - y) * np.cos(theta)
         e3 = 0#self.theta_d[T] - theta
@@ -135,8 +178,7 @@ class Trajectory_control():
     def  stampa(self):
         self.get_error()
         print (self.err)
-        #rospy.loginfo('Errore: %s', self.err)
-        #rospy.loginfo('Posizione Corrente: %s', self.q)
+
     
     #postprocessing 
     def publish(self):
@@ -148,54 +190,30 @@ class Trajectory_control():
         self.msg1.angular.y = 0.0
         self.msg1.angular.z = 0.0
         
-        # rospy.loginfo('INVIO DATI')
-        # rospy.loginfo(self.msg1)
-        # rospy.loginfo(self.deltasx)
-        # rospy.loginfo(self.deltadx)
-
-        # rospy.loginfo('POSIZIONE CORRENTE')
-        # rospy.loginfo(self.q[0])
-        # rospy.loginfo(self.q[1])
-        
         self.left_pub.publish(self.deltasx) 
         self.right_pub.publish(self.deltadx)
         self.twist_pub.publish(self.msg1)
-        #self.rate.sleep()
-        #self.get_error()
+ 
         
    
     
     def sterzata(self):
-        if abs(self.w) < 0.01:
+        if abs(self.w) < 0.05:
             self.deltadx = 0
             self.deltasx = 0 
         else:
-            R = self.v/self.w
-            self.deltadx = np.arctan(self.b/R)
+            R = (self.v/self.w)
+            self.deltadx = np.arctan(self.b_meter/R)
             self.deltasx = self.deltadx
         
         
 
 if __name__ == "__main__":
     try:
-        """
-        tc=Trajectory_control()
-        tc.get_error()
-        while (abs(tc.q[0]-tc.x_d) > tc.tol or abs(tc.q[1]-tc.y_d) > tc.tol):
-            u_t = un.control(tc.err, tc.v_d, tc.w_d)
-            tc.v = tc.v_d*np.cos(tc.err[2])-u_t[0]
-            tc.w = tc.w_d-u_t[1]
-            tc.w = -tc.w
-            tc.sterzata()
-            tc.publish()
         
-        tc.stampa()
-        """
         tc=Trajectory_control()
         tc.t = np.linspace(0, 100, 1000)
-        trajectory = "cyrcular"  #cubic, eight, cyrcular
-        tc.trajectory_generation(trajectory)
-        #tc.unicicle_nonLinear_control()
+        tc.trajectory_generation()
         tc.unicycle_linearized_control()
 
     except rospy.ROSInterruptException:
--- src/trajectory_generation.py
+++ src/trajectory_generation.py
@@ -3,46 +3,7 @@ import numpy as np
 
 #Trajectory generation
 class Trajectory_generation():
-
-    def cubic_trajectory(self, q_i, q_f, k, t=None):
-        x_i = q_i[0]
-        y_i = q_i[1]
-        x_f = q_f[0]
-        y_f = q_f[1]
-        if t is not None:
-            s = t/t[-1]
-            tau = 1/t[-1]
-        else:
-            s = np.linspace(0,1,200)
-            tau = 1
-            
-        b_x = k*np.cos(q_i[2]) + 3*q_i[0]
-        b_y = k*np.sin(q_i[2]) + 3*q_i[1]
-
-        a_x = k*np.cos(q_f[2]) - 3*q_f[0]
-        a_y = k*np.sin(q_f[2]) - 3*q_f[1]
-
-        #Cartesian cubic path 
-        x = x_f*s**3 - x_i*(s-1)**3 + a_x * s**2 * (s-1) + b_x * s * (s-1)**2
-        y = y_f*s**3 - y_i*(s-1)**3 + a_y * s**2 * (s-1) + b_y * s * (s-1)**2
-
-        #Compute first derivative
-        xp = 3*x_f*s**2 - 3*x_i*(s-1)**2 + a_x*(3*s**2 -2*s) + b_x*(s-1)*(3*s-1)
-        yp = 3*y_f*s**2 - 3*y_i*(s-1)**2  +a_y*(3*s**2 -2*s) + b_y*(s-1)*(3*s-1)
-        
-        #We can compute the geometric velocity and the posture angle theta
-        v = np.sqrt(xp**2 + yp**2)
-        theta = np.arctan2(yp,xp)
-        
-        #Compute second derivative
-        xpp = 6*x_f*s - 6*x_i*(s-1) + a_x*(6*s-2) + b_x*(6*s-4)
-        ypp = 6*y_f*s - 6*y_i*(s-1) + a_y*(6*s-2) + b_y*(6*s-4)
-        
-        #Compute the angular velocity
-        w = (ypp*xp - xpp*yp)/(v**2)
-        
-        return [x, y, v, w, theta]
-
+   
     def cyrcular_trajectory(self, t):
         
         A = [0.0, 0.0]
@@ -78,7 +39,7 @@ class Trajectory_generation():
         i = 0
         k = []
         for element in x_d:
-            if x_d[i] < 0 and (y_d[i] < 0 and y_d[i] > 2.15): 
+            if x_d[i] < 0 or y_d[i] < 0 or y_d[i] > 2.15: 
                 k.append(i)
             i = i+1
         
@@ -87,7 +48,7 @@ class Trajectory_generation():
         dotx_d = np.delete(dotx_d,k)
         doty_d = np.delete(doty_d,k)
 
-        print(x_d)
+        print(np.shape(x_d))
         print(y_d)
 
         v_d = np.sqrt(dotx_d**2 + doty_d**2)
@@ -95,14 +56,4 @@ class Trajectory_generation():
         w_d = w_d_val * np.ones(len(t))
         return [x_d, y_d, v_d, w_d, theta_d, dotx_d, doty_d]
 
-    def eight_trajectory(self, t):
-        x_c = 0
-        y_c = 0
-        R = 3
-        w_d = 1./15  # Desired Angular speed
-        x_d = x_c + R*np.sin(2*w_d*t) 
-        y_d = x_c + R*np.sin(w_d*t) 
-        
-        dotx_d = 2*R*w_d*np.cos(2*w_d*t)
-        doty_d = R*w_d*np.cos(w_d*t)
-        return [x_d,y_d,dotx_d, doty_d]
+    
